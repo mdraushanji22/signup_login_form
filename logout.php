2c3fe2415e2c6acc1a52658554ca2990c1d67116
mdraushan <mdraushanji22@gmail.com>
2026-02-15 00:00:30 +0530

    login-system

--- logout.php
+++ logout.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+session_unset();
+session_destroy();
+header("Location: index.php");
+exit();
 ?>
+
 
 <!DOCTYPE html>
 <html>
